748737472f9f25ff44d4d937b194bb24bdb522ec
laurauzcategui <luzcategui@GBP4682210.corp.sct.com>
2015-11-11 15:38:59 +0000

    Working on validity for autos

--- lesson3/validity.py
+++ lesson3/validity.py
@@ -1,16 +1,16 @@
 """
 Your task is to check the "productionStartYear" of the DBPedia autos datafile for valid values.
 The following things should be done:
-- check if the field "productionStartYear" contains a year
-- check if the year is in range 1886-2014
+- check if the field "productionStartYear" contains a year - done
+- check if the year is in range 1886-2014  - done
 - convert the value of the field to be just a year (not full datetime)
 - the rest of the fields and values should stay the same
 - if the value of the field is a valid year in range, as described above,
   write that line to the output_good file
 - if the value of the field is not a valid year,
   write that line to the output_bad file
-- discard rows (neither write to good nor bad) if the URI is not from dbpedia.org
-- you should use the provided way of reading and writing data (DictReader and DictWriter)
+- discard rows (neither write to good nor bad) if the URI is not from dbpedia.org  -- done
+- you should use the provided way of reading and writing data (DictReader and DictWriter) -- reader (DONE)
   They will take care of dealing with the header.
 
 You can write helper functions for checking the data and writing the files, but we will call only the
@@ -18,6 +18,7 @@ You can write helper functions for checking the data and writing the files, but 
 """
 import csv
 import pprint
+import re
 
 INPUT_FILE = 'autos.csv'
 OUTPUT_GOOD = 'autos-valid.csv'
@@ -28,18 +29,24 @@ def process_file(input_file, output_good, output_bad):
     with open(input_file, "r") as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
-
+        #remove URI not from dbpedia and header
+        valid_rows = [row for row in reader if row['URI'].startswith("http://dbpedia.org") and row not in header]
+        #remove rows with not valid years and within range 1886-2014
+        valid_years = [year for year in valid_rows if re.search('[0-9]{4}.*', \
+        year["productionStartYear"]) is not None and int(year["productionStartYear"][0:4]) in range(1886,2014)]
+        # transforming only to be year
+        #transfor_years = [year["productionStartYear"] for year in valid_years]
         #COMPLETE THIS FUNCTION
 
 
 
     # This is just an example on how you can use csv.DictWriter
     # Remember that you have to output 2 files
-    with open(output_good, "w") as g:
-        writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
-        writer.writeheader()
-        for row in YOURDATA:
-            writer.writerow(row)
+    #with open(output_good, "w") as g:
+    #    writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
+    #    writer.writeheader()
+    #    for row in YOURDATA:
+    #        writer.writerow(row)
 
 
 def test():
